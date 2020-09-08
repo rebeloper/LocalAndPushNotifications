264dfab2e16f53c72c321cf13a2ed84c4756b416
Alex Nagy <therebeloper@gmail.com>
2020-09-08 23:01:03 +0300

    resources

--- LocalAndPushNotifications/ViewController.swift
+++ LocalAndPushNotifications/ViewController.swift
@@ -38,3 +38,7 @@ class ViewController: UIViewController {
 // https://github.com/yonaskolb/mint
 // https://brew.sh
 
+// https://firebase.google.com/docs/cloud-messaging/ios/client
+// https://firebase.google.com/docs/functions/callable
+// Push notifications Typescript cloud function: https://rebeloper.com/mentoring
+
