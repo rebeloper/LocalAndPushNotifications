27ebbba0d1d3f334c53a34a2c31dfe17da4a969d
Alex Nagy <therebeloper@gmail.com>
2020-09-08 20:50:32 +0300

    local notifications

---
+++ LocalAndPushNotifications/LocalNotificationManager.swift
@@ -0,0 +1,91 @@
+//
+//  LocalNotificationManager.swift
+//  LocalAndPushNotifications
+//
+//  Created by Alex Nagy on 08/09/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+import UIKit
+
+struct LocalNotification {
+    var id: String
+    var title: String
+    var body: String
+}
+
+enum LocalNotificationDurationType {
+    case days
+    case hours
+    case minutes
+    case seconds
+}
+
+struct LocalNotificationManager {
+    
+    static private var notifications = [LocalNotification]()
+    
+    static private func requestPermission() -> Void {
+        UNUserNotificationCenter
+            .current()
+            .requestAuthorization(options: [.alert, .badge, .alert]) { granted, error in
+                if granted == true && error == nil {
+                    // We have permission!
+                }
+        }
+    }
+    
+    static private func addNotification(title: String, body: String) -> Void {
+        notifications.append(LocalNotification(id: UUID().uuidString, title: title, body: body))
+    }
+    
+    static private func scheduleNotifications(_ durationInSeconds: Int, repeats: Bool) {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        for notification in notifications {
+            let content = UNMutableNotificationContent()
+            content.title = notification.title
+            content.body = notification.body
+            content.sound = UNNotificationSound.default
+            content.badge = NSNumber(value: UIApplication.shared.applicationIconBadgeNumber + 1)
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(durationInSeconds), repeats: repeats)
+            let request = UNNotificationRequest(identifier: notification.id, content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+            UNUserNotificationCenter.current().add(request) { error in
+                guard error == nil else { return }
+                print("Scheduling notification with id: \(notification.id)")
+            }
+        }
+        notifications.removeAll()
+    }
+    
+    static private func scheduleNotifications(_ duration: Int, of type: LocalNotificationDurationType, repeats: Bool) {
+        var seconds = 0
+        switch type {
+        case .seconds:
+            seconds = duration
+        case .minutes:
+            seconds = duration * 60
+        case .hours:
+            seconds = duration * 60 * 60
+        case .days:
+            seconds = duration * 60 * 60 * 24
+        }
+        scheduleNotifications(seconds, repeats: repeats)
+    }
+    
+    static func cancel() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    }
+    
+    static func setNotification(_ duration: Int, of type: LocalNotificationDurationType, repeats: Bool, title: String, body: String) {
+        requestPermission()
+        addNotification(title: title, body: body)
+        scheduleNotifications(duration, of: type, repeats: repeats)
+    }
+
+}
+
--- LocalAndPushNotifications/ViewController.swift
+++ LocalAndPushNotifications/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapLocalNotificationButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Local Notification", message: nil, preferredStyle: .actionSheet)
+        let setLocalNotificationAction = UIAlertAction(title: "Set", style: .default) { (action) in
+            LocalNotificationManager.setNotification(5, of: .seconds, repeats: false, title: "Test", body: "Hello test")
+        }
+        let removeLocalNotificationAction = UIAlertAction(title: "Remove", style: .default) { (action) in
+            LocalNotificationManager.cancel()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in }
+        alertController.addAction(setLocalNotificationAction)
+        alertController.addAction(removeLocalNotificationAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
