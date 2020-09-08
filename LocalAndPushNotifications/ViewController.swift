84bb9537e2cc7853b34bbea889f87d8313099d14
Alex Nagy <therebeloper@gmail.com>
2020-09-08 19:59:38 +0300

    Initial Commit

---
+++ LocalAndPushNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalAndPushNotifications
+//
+//  Created by Alex Nagy on 08/09/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
